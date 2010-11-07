24dc5f25e41b2a6d3d0ac690aa8023bc0f9cc6ee
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-11-07 13:24:08 +0100

    rspec not working

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 
 group :development do
   gem 'rspec-rails'
+  gem 'annotate-models'
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+# Schema version: 20101015152412
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  :presence => true,
+                    :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format => { :width => email_regex }
+end
