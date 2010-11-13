29c2e3a7067f413737dac1ca9fafa6b379b236a0
Robert Riemann <robert.riemann@physik.hu-berlin.de>
2010-11-13 20:31:26 +0100

    better use of spec_helper

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -48,10 +48,7 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,4 +28,11 @@ RSpec.configure do |config|
   def test_sign_in(user)
     controller.sign_in(user)
   end
+
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+  end
 end
